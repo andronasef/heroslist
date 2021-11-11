8c3d61deac722b1aa4450bf943443f576a8d7b05
Andrew Nasef <androamazing@gmail.com>
2021-11-11 21:00:54 +0200

    Init Test Pages

---
+++ Code/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Checklist from './Checklist.svelte'
+  import ListsMenu from './ListsMenu.svelte'
+</script>
+
+<div class="flex justify-center items-center flex-col h-screen space-y-5">
+  <div class="title text-4xl text-$base-color uppercase">
+    Hereos
+    <span class="text-$second-color "> List</span>
+  </div>
+  <ListsMenu />
+  <Checklist />
+</div>
+
+<!-- <h1>{Lists.list0[0]}</h1> -->
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
+
+  .title {
+    font-family: 'Fredoka One', cursive;
+  }
+</style>
---
+++ Code/src/ListsMenu.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Icon from '@iconify/svelte'
+
+  let lists = [
+    ['ic:round-people-alt', ''],
+    ['fa-solid:bible', ''],
+    ['vs:kakaotalk', ''],
+    ['mdi:bike', ''],
+    ['fa-solid:home', ''],
+  ]
+</script>
+
+<div class="flex justify-center items-center gap-5 font-child text-white">
+  {#each lists as link}
+    <div
+      class=" bg-$base-color rounded-md w-11 h-11 cursor-pointer flex justify-center items-center text-w"
+    >
+      <span class="text-xl"> <Icon icon={link[0]} /> </span>
+    </div>
+  {/each}
+</div>
